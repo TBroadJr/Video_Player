daa45c0b5027ddf716733e77cc710eb435be0694
Tornelius <tkbjr28@icloud.com>
2022-12-30 11:21:00 -0500

    Initial Commit

---
+++ Video_Player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Video_Player
+//
+//  Created by Tornelius Broadwater, Jr on 12/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
